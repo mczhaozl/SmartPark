135a9cff974418f06ad058960a89033f0006e9d7
DESKTOP-UDF300U\phy <1769958355@qq.com>
2020-07-16 18:41:06 +0800

    字典及选择器

--- ant-design-vue-jeecg/src/views/modules/parking/modules/PkCarModal.vue
+++ ant-design-vue-jeecg/src/views/modules/parking/modules/PkCarModal.vue
@@ -14,17 +14,17 @@
         <a-form-item label="车牌号" :labelCol="labelCol" :wrapperCol="wrapperCol">
           <a-input v-decorator="['carNumber']" placeholder="请输入车牌号"></a-input>
         </a-form-item>
-        <a-form-item label="车辆类型，新能源" :labelCol="labelCol" :wrapperCol="wrapperCol">
-          <a-input v-decorator="['type']" placeholder="请输入车辆类型，新能源"></a-input>
+        <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="车辆类型">
+          <j-dict-select-tag placeholder="请选择车辆类型" :triggerChange="true" dictCode="car_type" v-decorator="['type']"/>
         </a-form-item>
         <a-form-item label="车辆颜色" :labelCol="labelCol" :wrapperCol="wrapperCol">
           <a-input v-decorator="['carColor']" placeholder="请输入车辆颜色"></a-input>
         </a-form-item>
-        <a-form-item label="用户Id" :labelCol="labelCol" :wrapperCol="wrapperCol">
+        <!-- <a-form-item label="用户Id" :labelCol="labelCol" :wrapperCol="wrapperCol">
           <a-input v-decorator="['userId']" placeholder="请输入用户Id"></a-input>
-        </a-form-item>
+        </a-form-item> -->
         <a-form-item label="用户名称" :labelCol="labelCol" :wrapperCol="wrapperCol">
-          <a-input v-decorator="['username']" placeholder="请输入用户名称"></a-input>
+          <j-select-user-by-dep placeholder="请选择用户" :multi="false" :multiple="false" v-decorator="['userId', {}]"/>
         </a-form-item>
         <a-form-item label="发动机号" :labelCol="labelCol" :wrapperCol="wrapperCol">
           <a-input v-decorator="['motor']" placeholder="请输入发动机号"></a-input>
@@ -43,12 +43,18 @@
   import { httpAction } from '@/api/manage'
   import pick from 'lodash.pick'
   import { validateDuplicateValue } from '@/utils/util'
-
+  import JDictSelectTag from '@/components/dict/JDictSelectTag'
+  import {getUserList } from '@/api/api'
+ 
+  import JSelectUserByDep from '@/components/jeecgbiz/JSelectUserByDep'
 
   export default {
     name: "PkCarModal",
     components: { 
-    },
+      JDictSelectTag,
+      JSelectUserByDep
+      
+   },
     data () {
       return {
         form: this.$form.createForm(this),
@@ -70,7 +76,8 @@
         url: {
           add: "/parking/pkCar/add",
           edit: "/parking/pkCar/edit",
-        }
+        },
+        selectedUser:[],
       }
     },
     created () {
